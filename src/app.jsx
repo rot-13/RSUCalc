1b1a342aa3f12cb3ee58a5e2c39945788eab60f7
Benny Weingarten-Gabbay <gardenofwine@gmail.com>
2016-11-27 17:30:57 +0200

    Add Result explanation (initial)

--- src/app.jsx
+++ src/app.jsx
@@ -18,18 +18,18 @@ var FieldClass = React.createClass({
       width: "calc(100% - 14px)",
       display: "inline"
     };
-    input = ( 
+    input = (
       <div>
       <span className="glyphicon glyphicon-asterisk text-danger" aria-hidden="true"/>
       <input style={inputStyle} ref="input" className="form-control" id={this.props.field}  placeholder={this.props.placeholder} value={this.state.value} onChange={this.handleChange} />
-      </div> 
+      </div>
       );
-      
+
     } else {
-    input = ( 
+    input = (
       <div>
       <input ref="input" className="form-control" id={this.props.field}  placeholder={this.props.placeholder} value={this.state.value} onChange={this.handleChange} />
-      </div> 
+      </div>
       );
     }
 
@@ -58,8 +58,8 @@ var SubmitButton = React.createClass({
     }
     return (
       <button type="submit" className="btn btn-default" disabled={this.props.waitingForServer}><span>חשב הכנסה ממכירה </span>{spinner}</button>
-    )    
-    
+    )
+
   }
 });
 
@@ -87,7 +87,7 @@ var CalculatorForm = React.createClass({
         }
         resultSubmit(error, result);
     });
-    this.setState({waitingForServer: true}); 
+    this.setState({waitingForServer: true});
   },
   onNumberOfSharesChange: function(newNumber) {
     this.props.updateNumberOfShares(newNumber);
@@ -111,12 +111,28 @@ var CalculatorForm = React.createClass({
 
 
 var Result = React.createClass({
+  getInitialState: function() {
+    return {displayingExplanation: false};
+  },
+  onExplanationToggle: function() {
+    this.setState({displayingExplanation: !this.state.displayingExplanation})
+  },
   render: function() {
     if (this.props.result) {
         if (this.props.result.eligibleFor102) {
-            return (<EligibleFor102Result result={this.props.result}/>);
+            return (
+              <span>
+                <EligibleFor102Result result={this.props.result}/>
+                <CalculationExplanationContainer onExplanationToggle={this.onExplanationToggle} result={this.props.result} displayingExplanation={this.state.displayingExplanation}/>
+              </span>
+              );
         } else {
-            return (<NonEligibleFor102Result result={this.props.result}/>);
+            return (
+              <span>
+                <NonEligibleFor102Result result={this.props.result}/>
+                <CalculationExplanationContainer onExplanationToggle={this.onExplanationToggle} result={this.props.result} displayingExplanation={this.state.displayingExplanation}/>
+              </span>
+              );
         }
     } else {
       return (<span/>);
@@ -128,14 +144,14 @@ var NonEligibleFor102Result = React.createClass({
   render: function() {
     if (this.props.result) {
         var res = this.props.result;
-        var shareValue = res.lastPrice.formatMoney(2, '.', ','); 
+        var shareValue = res.lastPrice.formatMoney(2, '.', ',');
         // var gainPerShare = res.totalGain.formatMoney(2, '.', ',');
         var totalGain = res.numberOfShares * res.totalGain;
         totalGain = totalGain.formatMoney(2, '.', ',');
         var gainPerShareWith102 = res.gainWith102.formatMoney(2, '.', ',');
         var totalGainWith102 = res.numberOfShares * gainPerShareWith102;
         totalGainWith102 = totalGainWith102.formatMoney(2, '.', ',');
-        
+
       var good = (
           <div>
           <div className="alert alert-warning" role="alert">אינך זכאית עדיין להטבת מס 102. הזכאות תתקיים בעוד {this.props.result.daysUntileligibleFor102} ימים.</div>
@@ -161,11 +177,11 @@ var EligibleFor102Result = React.createClass({
   render: function() {
     if (this.props.result) {
         var res = this.props.result;
-        var shareValue = res.lastPrice.formatMoney(2, '.', ','); 
+        var shareValue = res.lastPrice.formatMoney(2, '.', ',');
         // var gainPerShare = res.totalGain.formatMoney(2, '.', ',');
         var totalGain = res.totalGain * res.numberOfShares;
         totalGain = totalGain.formatMoney(2, '.', ',');
-        
+
       var good = (
           <div>
           <div className="alert alert-success" role="alert">זכאית להטבת מס 102</div>
@@ -184,15 +200,64 @@ var EligibleFor102Result = React.createClass({
   }
 });
 
+var ToggleCalcExplanation = React.createClass({
+  getDefaultProps: function() {
+    return {
+      buttonDisplayText: "צפייה בפרטי החישוב",
+      buttonCloseText: "סגור פרטי חישוב"
+    };
+  },
+  render: function() {
+    var buttonText = null;
+    if (this.props.displayingExplanation) {
+      buttonText = this.props.buttonCloseText;
+    } else {
+      buttonText = this.props.buttonDisplayText;
+    }
+
+    return (
+      <button type="submit" onClick={this.props.onToggle} className="btn btn-default" >{buttonText}</button>
+    )
+  }
+})
+
 var CalculationExplanation = React.createClass({
   render: function() {
-    
-  }  
+    if (!this.props.displayingExplanation) {
+      return null;
+    } else {
+      console.log(this.props.result);
+      return (
+        <table className="table">
+          <tbody>
+            <tr>
+              <td>מחיר בסיס</td>
+              <td>{this.props.result.costBasis}</td>
+            </tr>
+          </tbody>
+        </table>
+      )
+    }
+  }
+})
+
+var CalculationExplanationContainer = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ToggleCalcExplanation onToggle={this.props.onExplanationToggle} displayingExplanation={this.props.displayingExplanation}/>
+        <CalculationExplanation displayingExplanation={this.props.displayingExplanation} result={this.props.result}/>
+      </div>
+    )
+  }
 })
 
 var CalculatorContainer = React.createClass({
   getInitialState: function() {
-    return {result: null};
+    return {
+      result: null,
+      displayingExplanation: false
+    };
   },
   onFormSubmit: function(error, result) {
     if (error) {
@@ -211,7 +276,7 @@ var CalculatorContainer = React.createClass({
     return (
       <div>
         <CalculatorForm data={this.props.data} onSubmit={this.onFormSubmit} updateNumberOfShares={this.onUpdateNumberOfShares}/>
-        <Result result={this.state.result}/>
+        <Result result={this.state.result} displayingExplanation={this.state.displayingExplanation}/>
       </div>
     )
   }
@@ -226,7 +291,7 @@ var initialData = {
 };
 window.debug = initialData;
 
-React.render(
+ReactDOM.render(
   <CalculatorContainer data={ initialData } />,
   document.getElementById('rsuForm')
 );
